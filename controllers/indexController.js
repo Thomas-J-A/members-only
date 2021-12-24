4d0020d70541f665b77b568caabe821f8f80fa73
Thomas Aspbury <tomaspbury@icloud.com>
2021-12-24 16:10:58 +0000

    Basic styles for all pages

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,6 +4,7 @@ exports.index_get = (req, res) => {
   // Retrieve all posts from database
   Post
     .find()
+    .sort({ timestamp: -1 })
     .populate('user')
     .exec((err, posts) => {
       if (err) { return next(err); }
